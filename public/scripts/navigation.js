a57e10602fac442fff8ae48ac95b692dd2012204
Sadness <95544008+sadgrlonline@users.noreply.github.com>
2025-07-18 12:25:56 -0400

    Deploy from source commit fe49d2a

--- public/scripts/navigation.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener("DOMContentLoaded", function () {
-  const menuToggle = document.getElementById("menu-toggle");
-  const container = document.querySelector(".container");
-
-  menuToggle.addEventListener("click", function () {
-    container.classList.toggle("sidebar-open");
-  });
-});
